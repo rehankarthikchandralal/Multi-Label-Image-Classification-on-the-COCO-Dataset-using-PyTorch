17c3337ad74c7a569b02c05f0cf4f49ba81aac09
Rehan <rehankarthik12@gmail.com>
2025-01-27 15:02:03 +0100

    The number of output labels should be 80 from 1 to 80 But i am getting 1 to 90 label numbers with some labels getting skipped (12, 26, 29, 30, 45, 66, 68, 69, 71)
    {1: 'person', 2: 'bicycle', 3: 'car', 4: 'motorcycle', 5: 'airplane', 6: 'bus', 7: 'train', 8: 'truck', 9: 'boat', 10: 'traffic light', 11: 'fire hydrant', 13: 'stop sign', 14: 'parking meter', 15: 'bench', 16: 'bird', 17: 'cat', 18: 'dog', 19: 'horse', 20: 'sheep', 21: 'cow', 22: 'elephant', 23: 'bear', 24: 'zebra', 25: 'giraffe', 27: 'backpack', 28: 'umbrella', 31: 'handbag', 32: 'tie', 33: 'suitcase', 34: 'frisbee', 35: 'skis', 36: 'snowboard', 37: 'sports ball', 38: 'kite', 39: 'baseball bat', 40: 'baseball glove', 41: 'skateboard', 42: 'surfboard', 43: 'tennis racket', 44: 'bottle', 46: 'wine glass', 47: 'cup', 48: 'fork', 49: 'knife', 50: 'spoon', 51: 'bowl', 52: 'banana', 53: 'apple', 54: 'sandwich', 55: 'orange', 56: 'broccoli', 57: 'carrot', 58: 'hot dog', 59: 'pizza', 60: 'donut', 61: 'cake', 62: 'chair', 63: 'couch', 64: 'potted plant', 65: 'bed', 67: 'dining table', 70: 'toilet', 72: 'tv', 73: 'laptop', 74: 'mouse', 75: 'remote', 76: 'keyboard', 77: 'cell phone', 78: 'microwave', 79: 'oven', 80: 'toaster', 81: 'sink', 82: 'refrigerator', 84: 'book', 85: 'clock', 86: 'vase', 87: 'scissors', 88: 'teddy bear', 89: 'hair drier', 90: 'toothbrush'}
    this is causing indexing error and it was fixed

--- test_programs/task_3/test.py
+++ test_programs/task_3/test.py
@@ -31,6 +31,18 @@ plt.rcParams['figure.figsize'] = (15, 15)
 # In[2]:
 
 image_ids_annotations = defaultdict(list)
+# Define COCO categories mapping
+category_mapping = {
+    1: 0, 2: 1, 3: 2, 4: 3, 5: 4, 6: 5, 7: 6, 8: 7, 9: 8, 10: 9, 11: 10, 
+    13: 11, 14: 12, 15: 13, 16: 14, 17: 15, 18: 16, 19: 17, 20: 18, 21: 19, 
+    22: 20, 23: 21, 24: 22, 25: 23, 27: 24, 28: 25, 31: 26, 32: 27, 33: 28, 
+    34: 29, 35: 30, 36: 31, 37: 32, 38: 33, 39: 34, 40: 35, 41: 36, 42: 37, 
+    43: 38, 44: 39, 46: 40, 47: 41, 48: 42, 49: 43, 50: 44, 51: 45, 52: 46, 
+    53: 47, 54: 48, 55: 49, 56: 50, 57: 51, 58: 52, 59: 53, 60: 54, 61: 55, 
+    62: 56, 63: 57, 64: 58, 65: 59, 67: 60, 70: 61, 72: 62, 73: 63, 74: 64, 
+    75: 65, 76: 66, 77: 67, 78: 68, 79: 69, 80: 70, 81: 71, 82: 72, 84: 73, 
+    85: 74, 86: 75, 87: 76, 88: 77, 89: 78, 90: 79
+}
 
 # Load annotations
 path = '/home/rehan/Projects/Pytorch_Image_Classification/coco/annotations/annotations/instances_train2017.json'
@@ -118,10 +130,12 @@ class COCOMultiLabelDataset(Dataset):
         labels = torch.zeros(80)  # Initialize a tensor of zeros for multi-label classification (80 different classes in COCO)
 
         # Iterate through each annotation to set the corresponding labels
+        
         for ann in anns:
-            category_id = ann['category_id'] # Extract the category ID from the annotation
-            labels[category_id-1] = 1.0
-
+            category_id = ann['category_id']
+            if category_id in category_mapping:  # Ensure ID is valid
+                mapped_idx = category_mapping[category_id]  # Map to 0-79 index
+                labels[mapped_idx] = 1.0
         return img, labels # Return the transformed image and its multi-label tensor
 
 # In[7]:
